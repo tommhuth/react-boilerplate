a527ea381e2b1863c0125519d741004bda4e8515
Tomm Huth <tomm.huth@makingwaves.com>
2019-09-23 00:55:48 +0200

    base boilerplate

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// polyfill
+import "../assets/styles/app.scss"
+
+import "core-js/stable" 
+
+import { Workbox } from "workbox-window"
+import React from "react"
+import ReactDOM from "react-dom"  
+import Config from "./Config"
+
+ReactDOM.render(
+    <>
+        <div>Hello, world</div>
+    </>,
+    document.getElementById("root")
+)
+
+
+if (Config.REGISTER_SERVICEWORKER) {
+    let worker = new Workbox("/serviceworker.js")
+       
+    worker.addEventListener("installed", e => {
+        console.log(`SW ${e.isUpdate ? "updated" : "installed"}`)
+    })
+    worker.register()
+}
